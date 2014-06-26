e889573f0be353cac79024ae7627dbaaacc577f1
Jason Shen <sikajs@gmail.com>
2014-06-26 07:58:59 +0700

    Initialize repo with bootstrappers (ruby 2.1.2 and rails 4.0)

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-groupme1
-========
+Just my practice
+
+### Style Guides
+
+* [Ruby Style Guide](https://github.com/bbatsov/ruby-style-guide)
+* [Rails Style Guide](https://github.com/bbatsov/rails-style-guide)
+* [Better Specs](http://betterspecs.org/)
+* [Rails Best Practices](http://rails-bestpractices.com/)
+
+### Speedup Tips
+
+* [Speed up the Rails Asset Pipeline precompile process](http://stackoverflow.com/questions/11390447/how-can-you-speed-up-the-rails-asset-pipeline-precompile-process/11390454#11390454)
+
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Groupme1::Application.config.secret_key_base = 'e618085105095921376192ea49c9116fb2a8c02651d5e78ffbdc1a2a374ab5dd02346905c18da4f627dd8df58e02675e6f1fa1b93d58ab161ca97b957bab4663'
